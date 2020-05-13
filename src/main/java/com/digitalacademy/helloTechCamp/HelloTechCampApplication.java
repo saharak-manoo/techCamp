caa581326998c300bc5848654b622d1a98c211ca
Saharak <Saharakmanoo@outlook.com>
2020-05-13 13:32:09 +0700

    [Init] project

---
+++ src/main/java/com/digitalacademy/helloTechCamp/HelloTechCampApplication.java
@@ -0,0 +1,13 @@
+package com.digitalacademy.helloTechCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTechCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTechCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalacademy/helloTechCamp/HelloTechCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalacademy.helloTechCamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTechCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
